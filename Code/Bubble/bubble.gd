a18bdbfcd80f247e77d9001b1a40dab5c4d34556
mrkrabsdollar <domschles07@gmail.com>
2024-10-28 15:53:06 -0700

    e

--- Code/Bubble/bubble.gd
+++ Code/Bubble/bubble.gd
@@ -1,5 +1,26 @@
-extends Node2D
+extends Area2D
 class_name Bubble
+
+var main: Main
+
+func _ready() -> void:
+	input_event.connect(OnClicked)
+	
 
 func _process(delta: float) -> void:
 	position.y -= 100 * delta
+	if position.y < 0:
+		queue_free()
+
+func OnClicked(viewport, event, shape_id): 
+	if event is InputEventMouseButton: 
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				print("clicked me!")
+				Pop()
+
+
+func Pop(): 
+	main.PlaySound(position)
+	main.UpdateScore(3)
+	queue_free()
