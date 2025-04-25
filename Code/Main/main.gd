c49eb0c48efdf83c74c10d81c5c8cec7a5174904
LandoLoses <landon.marr@pcc.edu>
2025-04-25 11:16:23 -0700

    bubblepop2

---
+++ Code/Main/main.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name Main
+
+const BUBBLE: PackedScene = preload("res://Bubble/Bubble.tscn")
+@onready var timer = $Timer
+
+func _ready():
+	timer.timeout.connect(OnTimedOut)
+
+func OnTimedOut():
+	var rando: int = randi_range(0, get_viewport().size.x)
+	var bubble_node = BUBBLE.instantiate()
+	bubble_node.position.x = rando
+	bubble_node.position.y = get_viewport().size.y
+	add_child(bubble_node)
