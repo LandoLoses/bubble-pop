deb3fba14243aadf4c73ac306fd4c2f5f7841880
LandoLoses <landon.marr@pcc.edu>
2025-05-14 11:04:41 -0700

    Pop & SFX

--- Code/Bubble/Bubble.gd
+++ Code/Bubble/Bubble.gd
@@ -1,7 +1,27 @@
-extends Node2D
+extends Area2D
 class_name Bubble
 
-var speed: float = 100.0
+var audio: AudioStreamPlayer2D 
+var speed: float = 200.0 # px / sec.
 
+func _ready():
+	input_event.connect(OnClicked) 
+	
 func _process(delta):
 	position.y -= speed * delta
+	if position.y < 0:
+		queue_free()
+
+func OnClicked(_viewport, event, _shape):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				Pop()
+				print("Player clicked down.")
+			else:
+				print("Player clicked up.")
+				
+func Pop():
+	audio.position = position
+	audio.play()
+	queue_free()
--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -3,6 +3,7 @@ class_name Main
 
 const BUBBLE: PackedScene = preload("res://Bubble/Bubble.tscn")
 @onready var timer = $Timer
+@onready var audio = $Audio
 
 func _ready():
 	timer.timeout.connect(OnTimedOut)
@@ -10,6 +11,7 @@ func _ready():
 func OnTimedOut():
 	var rando: int = randi_range(0, get_viewport().size.x)
 	var bubble_node = BUBBLE.instantiate()
+	bubble_node.audio = audio
 	bubble_node.position.x = rando
 	bubble_node.position.y = get_viewport().size.y
 	add_child(bubble_node)
