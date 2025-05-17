a7b2a87a6863431592812b8522e43e8f1df6c27e
LandoLoses <landon.marr@pcc.edu>
2025-05-17 11:36:34 -0700

    Finished Bubble Pop

--- Code/Bubble/Bubble.gd
+++ Code/Bubble/Bubble.gd
@@ -1,6 +1,7 @@
 extends Area2D
 class_name Bubble
 
+var main: Main #null
 var audio: AudioStreamPlayer2D 
 var speed: float = 200.0 # px / sec.
 
@@ -17,11 +18,11 @@ func OnClicked(_viewport, event, _shape):
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				Pop()
-				print("Player clicked down.")
-			else:
-				print("Player clicked up.")
-				
+
+
 func Pop():
 	audio.position = position
+	audio.pitch_scale = randf_range(0.8, 1.2)
 	audio.play()
+	main.IncreaseScore()
 	queue_free()
--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -2,16 +2,32 @@ extends Node
 class_name Main
 
 const BUBBLE: PackedScene = preload("res://Bubble/Bubble.tscn")
+
 @onready var timer = $Timer
 @onready var audio = $Audio
+@onready var score_ui: Label = $UI/ScoreUI
+
+var score: int = 0
 
 func _ready():
 	timer.timeout.connect(OnTimedOut)
+	score_ui.text = str(score)
+
+func _unhandled_key_input(event):
+	if event is InputEventKey:
+		if event.keycode == KEY_ESCAPE and event.pressed:
+			get_tree().quit()
 
 func OnTimedOut():
 	var rando: int = randi_range(0, get_viewport().size.x)
 	var bubble_node = BUBBLE.instantiate()
+	bubble_node.main = self
 	bubble_node.audio = audio
 	bubble_node.position.x = rando
 	bubble_node.position.y = get_viewport().size.y
 	add_child(bubble_node)
+
+
+func IncreaseScore():
+	score += 1
+	score_ui.text = str(score)
